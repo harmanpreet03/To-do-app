b8f53a661ac031978fdb9ff1a786e04a25f2cdc9
harmanpreet ghotra <harmanpreetghotra17@gmail.com>
2023-09-08 10:48:06 -0400

    final edits

--- src/App.jsx
+++ src/App.jsx
@@ -7,37 +7,102 @@ const App = () => {
   const [todoItem, setToDoItem] = useState("");
   const [todoList, setTodoList] = useState([]);
   const [day, setDay] = useState("today");
+  const [editingItemId, setEditingItemId] = useState(null); 
+  const [editedText, setEditedText] = useState(""); 
 
-  useEffect(() => {
-    fetch(`http://localhost:8085/${day}`)
-      .then((response) => response.json())
-      .then((data) => {
-        setTodoList(data);
-      });
-  }, [day]);
-
-  const onFormSubmit = (e) => {
-    e.preventDefault();
-    const newItem = {
-      item: todoItem,
-    };
-    fetch(`http://localhost:8085/${day}`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "Accept": "application/json",
-      },
-      body: JSON.stringify(newItem),
-    })
-      .then((response) => response.json())
-      .then((todoItem) => setTodoList([...todoList, todoItem]));
+  const fetchData = async () => {
+    try {
+      const response = await fetch(`http://localhost:8085/${day}`);
+      if (!response.ok) throw new Error("Network response was not ok");
+      const data = await response.json();
+      setTodoList(data);
+    } catch (error) {
+      console.error(error);
+    }
   };
 
+  useEffect(() => {
+    fetchData();
+  }, [day]);
+
+  const onFormSubmit = async (e) => {
+    e.preventDefault();
+    const newItem = { item: todoItem };
+
+    try {
+      const response = await fetch(`http://localhost:8085/${day}`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify(newItem),
+      });
+
+      if (!response.ok) throw new Error("Network response was not ok");
+
+      const newItemData = await response.json();
+      setTodoList([...todoList, newItemData]);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+ 
   const onChangeText = (e) => setToDoItem(e.target.value);
+
+  
+  const onDeleteItem = async (itemId) => {
+    try {
+      const response = await fetch(`http://localhost:8085/${day}/${itemId}`, {
+        method: "DELETE",
+      });
+
+      if (!response.ok) throw new Error("Network response was not ok");
+
+      const updatedTodoList = todoList.filter((item) => item.id !== itemId);
+      setTodoList(updatedTodoList);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+
+  const onUpdateItem = async (itemId) => {
+    try {
+      const response = await fetch(`http://localhost:8085/${day}/${itemId}`, {
+        method: "PUT", 
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify({ item: editedText }), 
+      });
+
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+
+      
+      fetchData();
+
+      
+      setEditingItemId(null);
+      setEditedText("");
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  
+  const onEditClick = (itemId, itemText) => {
+    setEditingItemId(itemId);
+    setEditedText(itemText);
+  };
 
   return (
     <div className="container">
-      <h1 className="todo-app">To-Do List</h1>
+      <h1 className="todo-app">To-Do List !!</h1>
       <div className="link-div">
         <Link to={"/yesterday"}>Yesterday</Link>
         <Link to={"/today"}>Today</Link>
@@ -48,7 +113,15 @@ const App = () => {
         onChangeText={onChangeText}
         todoItem={todoItem}
       />
-      <DoList todoList={todoList} />
+      <DoList
+        todoList={todoList}
+        onDeleteItem={onDeleteItem}
+        onEditClick={onEditClick}
+        editingItemId={editingItemId}
+        editedText={editedText}
+        setEditedText={setEditedText}
+        onUpdateItem={onUpdateItem}
+      />
     </div>
   );
 };
--- src/components/Dolist.jsx
+++ src/components/Dolist.jsx
@@ -1,12 +1,40 @@
 import React from "react";
 import Onecard from "./Onecard";
 
-const DoList = ({ todoList }) => {
+const DoList = ({
+  todoList,
+  onDeleteItem,
+  onEditClick,
+  editingItemId,
+  editedText,
+  setEditedText,
+  onUpdateItem,
+}) => {
   return (
     <div className="task-container">
       <ul className="Onecard">
-        {todoList.map((item, index) => (
-          <Onecard key={index}>{item.item}</Onecard>
+        {todoList.map((item) => (
+          <Onecard key={item.id}>
+            {item.id === editingItemId ? (
+              // Display the input field for editing
+              <div>
+                <input
+                  type="text"
+                  value={editedText}
+                  onChange={(e) => setEditedText(e.target.value)}
+                />
+                <button onClick={() => onUpdateItem(item.id)}>Save</button>
+              </div>
+            ) : (
+              <div>
+                <span>{item.item}</span>
+                <button onClick={() => onEditClick(item.id, item.item)}>
+                  Edit
+                </button>
+                <button onClick={() => onDeleteItem(item.id)}>Delete</button>
+              </div>
+            )}
+          </Onecard>
         ))}
       </ul>
     </div>
